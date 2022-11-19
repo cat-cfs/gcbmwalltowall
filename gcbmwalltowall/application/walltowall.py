a0a74a73fc318cc6b9b414d1aa9fb2f4e922d759
mfellows <mfellows@nrcan.gc.ca>
2022-11-18 16:16:44 -0800

    Added inventory merge - two or more projects that have been run through "walltowall prepare" can be merged together using the spatial rollback tool's merge feature:
    walltowall merge <path to walltowall config file> <project 1 dir> <project 2 dir> [project n dir...] --output_path <merged output path>

--- gcbmwalltowall/application/walltowall.py
+++ gcbmwalltowall/application/walltowall.py
@@ -1,9 +1,17 @@
 import logging
 import sys
+from psutil import virtual_memory
+from pathlib import Path
 from argparse import ArgumentParser
+from tempfile import TemporaryDirectory
+from spatial_inventory_rollback.gcbm.merge import gcbm_merge
+from spatial_inventory_rollback.gcbm.merge import gcbm_merge_tile
+from spatial_inventory_rollback.gcbm.merge.gcbm_merge_input_db import replace_direct_attached_transition_rules
 from gcbmwalltowall.builder.projectbuilder import ProjectBuilder
 from gcbmwalltowall.configuration.configuration import Configuration
+from gcbmwalltowall.configuration.gcbmconfigurer import GCBMConfigurer
 from gcbmwalltowall.component.project import Project
+from gcbmwalltowall.component.preparedproject import PreparedProject
 
 def build(args):
     ProjectBuilder.build_from_file(args.config_path, args.output_path)
@@ -23,6 +31,38 @@ def prepare(args):
     project.configure_gcbm(config.gcbm_template_path,
                            config.gcbm_disturbance_order,
                            **extra_args)
+
+def merge(args):
+    with TemporaryDirectory() as tmp:
+        projects = [PreparedProject(path) for path in args.project_paths]
+        inventories = [project.prepare_merge(tmp, i) for i, project in enumerate(projects)]
+
+        output_path = Path(args.output_path)
+        merged_output_path = output_path.joinpath("layers", "merged")
+        tiled_output_path = output_path.joinpath("layers", "tiled")
+        db_output_path = output_path.joinpath("input_database")
+        
+        start_year = min((project.start_year for project in projects))
+        end_year = max((project.end_year for project in projects))
+
+        memory_limit = virtual_memory().available * 0.75 // 1024**2
+        merged_data = gcbm_merge.merge(
+            inventories, str(merged_output_path), str(db_output_path),
+            start_year, memory_limit_MB=memory_limit)
+
+        gcbm_merge_tile.tile(str(tiled_output_path), merged_data, inventories)
+        replace_direct_attached_transition_rules(
+            str(db_output_path.joinpath("gcbm_input.db")),
+            str(tiled_output_path.joinpath("transition_rules.csv")))
+
+        config = Configuration.load(args.config_path, args.output_path)
+        configurer = GCBMConfigurer(
+            [str(tiled_output_path)], config.gcbm_template_path,
+            str(db_output_path.joinpath("gcbm_input.db")),
+            str(output_path.joinpath("gcbm_project")), start_year, end_year,
+            config.gcbm_disturbance_order)
+    
+        configurer.configure()
 
 def run(args):
     print(args)
@@ -60,11 +100,25 @@ def cli():
         "output_path", nargs="?",
         help="destination directory for project files")
 
+    merge_parser = subparsers.add_parser(
+        "merge",
+        help="Merge two or more walltowall-prepared inventories together.")
+    merge_parser.set_defaults(func=merge)
+    merge_parser.add_argument(
+        "config_path",
+        help="path to walltowall config file for disturbance order and GCBM config templates")
+    merge_parser.add_argument(
+        "project_paths", nargs="+",
+        help="root directories of at least two walltowall-prepared projects")
+    merge_parser.add_argument(
+        "--output_path", required=True,
+        help="path to generate merged output in")
+
     run_parser = subparsers.add_parser(
         "run", help="Run the specified project either locally or on the cluster.")
+    run_parser.set_defaults(func=run)
     run_parser.add_argument(
         "host", choices=["local", "cluster"], help="run either locally or on the cluster")
-    run_parser.set_defaults(func=run)
 
     args = parser.parse_args()
     args.func(args)
---
+++ gcbmwalltowall/component/preparedproject.py
@@ -0,0 +1,110 @@
+import json
+import shutil
+from datetime import datetime
+from pathlib import Path
+from spatial_inventory_rollback.gcbm.merge.gcbm_merge_layer_input import MergeInputLayers
+from gcbmwalltowall.configuration.gcbmconfigurer import GCBMConfigurer
+
+class PreparedProject:
+
+    def __init__(self, path):
+        self.path = Path(path).absolute()
+
+    @property
+    def tiled_layer_path(self):
+        return self.path.joinpath("layers/tiled")
+
+    @property
+    def rollback_layer_path(self):
+        rollback_layer_path = self.path.joinpath("layers/rollback")
+        return rollback_layer_path if rollback_layer_path.exists() else None
+
+    @property
+    def input_db_path(self):
+        return self.path.joinpath("input_database/gcbm_input.db")
+
+    @property
+    def rollback_db_path(self):
+        rollback_db_path = self.path.joinpath("input_database/rollback_gcbm_input.db")
+        return rollback_db_path if rollback_db_path.exists() else None
+
+    @property
+    def gcbm_config_path(self):
+        return self.path.joinpath("gcbm_project")
+
+    @property
+    def has_rollback(self):
+        return self.rollback_layer_path is not None
+
+    @property
+    def start_year(self):
+        config = json.load(open(self.gcbm_config_path.joinpath("localdomain.json")))
+        return datetime.strptime(config["LocalDomain"]["start_date"], "%Y/%m/%d").year
+
+    @property
+    def end_year(self):
+        config = json.load(open(self.gcbm_config_path.joinpath("localdomain.json")))
+        return datetime.strptime(config["LocalDomain"]["end_date"], "%Y/%m/%d").year
+
+    @property
+    def all_study_area_layers(self):
+        layer_metadata = {}
+        for layer_path in (self.tiled_layer_path, self.rollback_layer_path):
+            if not layer_path:
+                continue
+
+            layer_metadata.update({
+                layer["name"]: layer for layer in
+                json.load(open(layer_path.joinpath("study_area.json")))["layers"]
+            })
+
+        return layer_metadata
+
+    @property
+    def configured_layers(self):
+        config = json.load(open(self.gcbm_config_path.joinpath("provider_config.json")))
+        return {
+            layer["name"]: layer for layer in
+            config["Providers"]["RasterTiled"]["layers"]
+        }
+
+    def prepare_merge(self, working_path, priority):
+        if not self.has_rollback:
+            transition_rules = self.tiled_layer_path.joinpath("transition_rules.csv")
+            
+            return MergeInputLayers(
+                priority,
+                str(self.input_db_path),
+                str(self.tiled_layer_path.joinpath("study_area.json")),
+                str(transition_rules) if transition_rules.exists() else None,
+                self.start_year,
+                priority == 0)
+
+        # Merge expects a single study_area.json, so for projects that have been
+        # rolled back, need to consolidate the layers and study areas.
+        staging_path = Path(working_path).joinpath(self.path.stem)
+        staging_path.mkdir()
+
+        staging_study_area = staging_path.joinpath("study_area.json")
+        shutil.copyfile(self.rollback_layer_path.joinpath("study_area.json"), staging_study_area)
+
+        all_study_area_layers = self.all_study_area_layers
+        with GCBMConfigurer.update_json_file(staging_study_area) as study_area:
+            study_area["layers"] = []
+            for layer_name, layer in self.configured_layers.items():
+                study_area["layers"].append(all_study_area_layers[layer_name])
+                for layer_file in (
+                    self.gcbm_config_path.joinpath(layer["layer_path"]),
+                    self.gcbm_config_path.joinpath(layer["layer_path"]).with_suffix(".json")
+                ):
+                    shutil.copyfile(layer_file, staging_path.joinpath(layer_file.name))
+
+        transition_rules = self.rollback_layer_path.joinpath("transition_rules.csv")
+
+        return MergeInputLayers(
+            priority,
+            str(self.rollback_db_path),
+            str(staging_study_area),
+            str(transition_rules) if transition_rules.exists() else None,
+            self.start_year,
+            priority == 0)
