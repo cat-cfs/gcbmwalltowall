a61e2c49777702459cc1360bfd8d1267fc650ed6
Max Fellows <max.fellows@nrcan.gc.ca>
2025-08-20 08:31:33 -0700

    Black formatting.

--- gcbmwalltowall/converter/layerconverter.py
+++ gcbmwalltowall/converter/layerconverter.py
@@ -11,15 +11,16 @@ from mojadata.config import GDAL_CREATION_OPTIONS
 from mojadata.util import gdal
 from mojadata.util.gdalhelper import GDALHelper
 
+
 class LayerConverter:
-    
+
     def __init__(self, *args, temp_dir: Path | str = None, **kwargs):
         if temp_dir:
             self._temp_dir = Path(temp_dir)
         else:
             self._temp_dir_ref = TemporaryDirectory()
             self._temp_dir = Path(self._temp_dir_ref.name)
-    
+
     def handles(self, layer: PreparedLayer) -> bool:
         raise NotImplementedError()
 
@@ -31,14 +32,14 @@ class LayerConverter:
 
 
 class DelegatingLayerConverter(LayerConverter):
-    
+
     def __init__(self, converters: list[LayerConverter], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._converters = converters
-    
+
     def handles(self, layer: PreparedLayer) -> bool:
         return any((c.handles(layer) for c in self._converters))
-    
+
     def convert_internal(self, layers: list[PreparedLayer]) -> list[RasterInputLayer]:
         results = []
         for subconverter in self._converters:
@@ -48,7 +49,7 @@ class DelegatingLayerConverter(LayerConverter):
 
 
 class DefaultLayerConverter(LayerConverter):
-    
+
     def __init__(self, name_remappings: dict[str, str] = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._name_remappings = name_remappings or {}
@@ -64,7 +65,7 @@ class DefaultLayerConverter(LayerConverter):
             "last_pass_disturbance_type",
             "mean_annual_temp",
         ]
-    
+
     def handles(self, layer: PreparedLayer) -> bool:
         _handles = layer.name not in {"initial_current_land_class"}
         return _handles
@@ -72,7 +73,7 @@ class DefaultLayerConverter(LayerConverter):
     def convert_internal(self, layers: list[PreparedLayer]) -> list[RasterInputLayer]:
         if not layers:
             return []
-        
+
         logging.info(f"Converting layers: {', '.join((l.name for l in layers))}")
 
         return [
@@ -81,14 +82,15 @@ class DefaultLayerConverter(LayerConverter):
                 [RasterInputSource(path=str(layer.path))],
                 self._build_attribute_table(layer),
                 self._get_tags(layer),
-            ) for layer in layers
+            )
+            for layer in layers
         ]
 
     def _build_attribute_table(self, layer: PreparedLayer) -> DataFrame:
         gcbm_attribute_table = layer.tiler_metadata.get("attributes")
         if not gcbm_attribute_table:
             return None
-        
+
         rows = []
         for att_id, att_value in gcbm_attribute_table.items():
             row = {"id": int(att_id)}
@@ -96,7 +98,7 @@ class DefaultLayerConverter(LayerConverter):
                 row.update({k: v for k, v in att_value.items() if k != "conditions"})
             else:
                 row.update({"value": att_value})
-            
+
             rows.append(row)
 
         return InMemoryAttributeTableReader(DataFrame(rows))
@@ -111,10 +113,10 @@ class DefaultLayerConverter(LayerConverter):
 
 
 class LandClassLayerConverter(LayerConverter):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     def handles(self, layer: PreparedLayer) -> bool:
         return layer.name == "initial_current_land_class"
 
@@ -122,9 +124,9 @@ class LandClassLayerConverter(LayerConverter):
         layer = next(iter(layers), None)
         if not layer:
             return []
-        
+
         logging.info(f"Converting layer: {layer.name}")
-        
+
         gcbm_cbm4_landclass_lookup = {
             "FL": 0,
             "CL": 1,
@@ -134,31 +136,36 @@ class LandClassLayerConverter(LayerConverter):
             "OL": 5,
             "UFL": 16,
         }
-        
+
         original_ndv = layer.tiler_metadata["nodata"]
         new_ndv = 32767
         px_remappings = {original_ndv: new_ndv}
-        px_remappings.update({
-            original_px: gcbm_cbm4_landclass_lookup.get(gcbm_landclass, new_ndv)
-            for original_px, gcbm_landclass in layer.tiler_metadata["attributes"].items()
-        })
+        px_remappings.update(
+            {
+                original_px: gcbm_cbm4_landclass_lookup.get(gcbm_landclass, new_ndv)
+                for original_px, gcbm_landclass in layer.tiler_metadata[
+                    "attributes"
+                ].items()
+            }
+        )
 
         old_px, new_px = list(zip(*px_remappings.items()))
         output_path = self._temp_dir.joinpath(f"{layer.name}.tif")
         GDALHelper.calc(
-            str(layer.path), str(output_path),
+            str(layer.path),
+            str(output_path),
             lambda d: np.select(
                 np.array(old_px)[:, None, None].astype(np.int16) == d.astype(np.int16),
-                new_px
+                new_px,
             ),
             data_type=gdal.GDT_Int16,
-            nodata_value=new_ndv
+            nodata_value=new_ndv,
         )
-        
+
         return [
             RasterInputLayer(
                 "land_class",
                 [RasterInputSource(path=str(output_path))],
-                tags=layer.study_area_metadata.get("tags")
+                tags=layer.study_area_metadata.get("tags"),
             )
         ]
