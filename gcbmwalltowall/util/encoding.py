7837158d01ddeb5518b2d77a6cc7727bf3dbd42f
Max Fellows <max.fellows@nrcan.gc.ca>
2026-02-10 13:02:56 -0800

    Encoding fix.

--- gcbmwalltowall/util/encoding.py
+++ gcbmwalltowall/util/encoding.py
@@ -1,25 +1,18 @@
 import json
 import pandas as pd
+from io import BytesIO
 from ftfy import fix_encoding, guess_bytes
 
 
+def read_text_file(path):
+    file_bytes = open(path, "rb").read()
+    fixed_bytes, _ = guess_bytes(file_bytes)
+    return fix_encoding(fixed_bytes)
+
+
 def load_json(json_path):
-    json_bytes = open(json_path, "rb").read()
-    fixed_bytes, _ = guess_bytes(json_bytes)
-    return json.loads(fix_encoding(fixed_bytes))
+    return json.loads(read_text_file(json_path))
 
 
 def load_csv(csv_path):
-    try:
-        return pd.read_csv(open(csv_path))
-    except:
-        return _load_bad_csv(csv_path)
-
-
-def _load_bad_csv(csv_path):
-    return pd.read_csv(
-        open(csv_path, encoding="utf-8-sig")
-            .read()
-            .replace("\n", "")
-            .replace("\t", " ")
-    )
+    return pd.read_csv(BytesIO(read_text_file(csv_path).encode()))
